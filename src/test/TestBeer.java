0172856395dbc7215fbd725c4cf8f7ad42babe32
Unknown <maximilian.wolf@edu.campus02.at>
2020-05-12 14:48:40 +0200

    Added TestBeer.java

--- src/test/TestBeer.java
+++ src/test/TestBeer.java
@@ -1,10 +1,22 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestBeer {
+    static Beer goesser, corona, guiness;
+
     /**
      * Setup method to create specific objects to test on
      */
     @BeforeClass
     public static void setup(){
-
+        goesser = new Beer(new Liquid("lager", 0.5, 6),
+                "goesser", false, 12);
+        corona = new Beer(new Liquid("lager", 0.5, 4),
+                "corona", false, 9);
+        guiness = new Beer(new Liquid("draught", 0.5, 7),
+                "guiness", true, 16);
     }
 
     /**
@@ -13,7 +25,7 @@ public class TestBeer {
      */
     @Test
     public void testList(){
-
+        assertEquals("goesser\ncorona\n", Beer.printLightBeers());
     }
 
     /**
@@ -22,7 +34,10 @@ public class TestBeer {
      */
     @Test
     public void checkException(){
-
+        assertThrows(IllegalArgumentException.class,()->{
+            Beer nonAlc = new Beer(new Liquid("lager", 0.5, 0),
+                    "NullKommaJosef", false, 0);
+        });
     }
 
     /**
@@ -31,7 +46,15 @@ public class TestBeer {
      */
     @Test
     public void testConstr(){
-
+        assertEquals("lager", corona.getType());
+        assertEquals("lager", goesser.getLiquid().getName());
+        assertEquals(0.5, goesser.getLiquid().getVolume());
+        assertEquals(6, goesser.getLiquid().getAlcoholPercent());
+        assertEquals(16, guiness.getWort());
+        for (Beer i: Beer.lightBeers) {
+            assertFalse(i.isDark());
+        }
+        assertTrue(guiness.isDark());
     }
 
     /**
@@ -40,7 +63,13 @@ public class TestBeer {
      */
     @Test
     public void testSetLiquid(){
-
+        Beer test = new Beer(new Liquid("test", 0.5, 6),
+                "test", false, 12);
+        Liquid lTest = new Liquid("new Name", 0.3, 5);
+        test.setLiquid(lTest);
+        assertEquals("new Name", test.getLiquid().getName());
+        assertEquals(0.3, test.getVolume());
+        assertEquals(5, test.getAlcoholPercent());
     }
 
     /**
@@ -49,7 +78,11 @@ public class TestBeer {
      */
     @Test
     public void testSetType(){
-
+        Beer test = new Beer(new Liquid("test", 0.5, 6),
+                "test", false, 12);
+        assertEquals("test", test.getType());
+        test.setType("Ale");
+        assertEquals("Ale", test.getType());
     }
 
     /**
@@ -58,7 +91,11 @@ public class TestBeer {
      */
     @Test
     public void testSetDark(){
-
+        Beer test = new Beer(new Liquid("test", 0.5, 6),
+                "test", false, 12);
+        assertFalse(test.isDark());
+        test.setDark(true);
+        assertTrue(test.isDark());
     }
 
     /**
@@ -67,7 +104,11 @@ public class TestBeer {
      */
     @Test
     public void testSetWort(){
-      
+        Beer test = new Beer(new Liquid("test", 0.5, 6),
+                "test", false, 12);
+        assertEquals(12, test.getWort());
+        test.setWort(25);
+        assertEquals(25, test.getWort());
     }
 
 }
