2e24d98aeca48854f4c7144eef0772c81c58cace
DominikSchick <dominik.schick@edu.campus02.at>
2020-05-12 10:58:34 +0200

    Added comments and empty methods to TestLiquid.java and TestSpirit.java

---
+++ src/test/TestLiquid.java
@@ -0,0 +1,33 @@
+/**
+ * test class
+ */
+ public class TestLiquid {
+   /**
+    * static variable test
+    */
+
+
+    /**
+     * will execute operation before each test
+     */
+     @BeforeClass
+     public static void setup() {
+
+     }
+
+     /**
+      * tests the setter for getVolume
+      */
+      @Test
+      public void testVolume() {
+
+      }
+
+      /**
+       * tests the setter for AlcoholPercent
+       */
+       @Test
+       public void testAlcoholPercent() {
+
+       }
+     }
