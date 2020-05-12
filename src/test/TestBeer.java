6d3c7ff0537bcb29c3c263c59d7b2b9f89d1f71f
Unknown <maximilian.wolf@edu.campus02.at>
2020-05-12 11:03:42 +0200

    Added comments and method headers to TestBeer.java

---
+++ src/test/TestBeer.java
@@ -0,0 +1,73 @@
+public class TestBeer {
+    /**
+     * Setup method to create specific objects to test on
+     */
+    @BeforeClass
+    public static void setup(){
+
+    }
+
+    /**
+     * A method to see if the List was created correctly and if the print
+     * method of Beer works correctly
+     */
+    @Test
+    public void testList(){
+
+    }
+
+    /**
+     * Check if our own exception throws correctly, in this case you should
+     * not be able to make a new beer with no alcohol
+     */
+    @Test
+    public void checkException(){
+
+    }
+
+    /**
+     * Create the beers with the constructor and fetch those expected
+     * outcomes with the getter methods
+     */
+    @Test
+    public void testConstr(){
+
+    }
+
+    /**
+     * Create a new beer and try to set the liquid of that specific beer to a
+     * new one, the outcome should be the new setted liquid
+     */
+    @Test
+    public void testSetLiquid(){
+
+    }
+
+    /**
+     * Create a new beer then set a new type of this beer and compare if
+     * the change happened
+     */
+    @Test
+    public void testSetType(){
+
+    }
+
+    /**
+     * Create a new beer then set a new color of this beer and compare if
+     * the change happened
+     */
+    @Test
+    public void testSetDark(){
+
+    }
+
+    /**
+     * Create a new beer then set a new wort of this beer and compare if
+     * the change happened
+     */
+    @Test
+    public void testSetWort(){
+      
+    }
+
+}
