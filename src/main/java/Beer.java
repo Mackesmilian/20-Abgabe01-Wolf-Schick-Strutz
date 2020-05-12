7e1b8a6d90a9beffe4e0632712e3ae56054dd1ec
thomasstrutz <62703418+thomasstrutz@users.noreply.github.com>
2020-05-12 09:49:02 +0200

    added the structur of the class and theire empty methods and commentetd it

---
+++ src/main/java/Beer.java
@@ -0,0 +1,124 @@
+/**
+ * Class that represents a beer with the attributes isDark, wort, the liquid,
+ * and liquid's attributes
+ */
+public class Beer{
+
+    /**
+     * Constructor for beer, also creates a list of pale beers.
+     *
+     * @param liquid corresponding liquid for the beer
+     * @param name   the beers name
+     * @param isDark a bool whether the bear is pale or dark
+     * @param wort   the beer's wort, aka strength in taste
+     * @throws IllegalArgumentException when Beer with alcohol content 0 is
+     *                                  created
+     */
+    public Beer(){
+    }
+
+    /**
+     * @return String with the names of the beers in the List lightBeers
+     */
+    public static String printLightBeers() {
+    }
+
+    /**
+     * Getter for name
+     *
+     * @return returns a String for the beer's name
+     */
+    public String getName() {
+    }
+
+    /**
+     * Getter for the corresponding liquid
+     *
+     * @return Object Liquid of the corresponding beer
+     */
+    public Liquid getLiquid() {
+    }
+
+    /**
+     * Sets a new liquid
+     *
+     * @param liquid New liquid to set
+     */
+    public void setLiquid() {
+    }
+
+    /**
+     * Gets the type of the beer, which is the liquid's name
+     *
+     * @return name of liquid
+     */
+    public String getType() {
+    }
+
+    /**
+     * Setter for type, which is the name of the liquid
+     *
+     * @param type String representing the liquid's name/beer type
+     */
+    public void setType() {
+    }
+
+    /**
+     * Checks if the beer is dark
+     *
+     * @return bool for whether the beer is dark or not
+     */
+    public boolean isDark() {
+    }
+
+    /**
+     * Setter for dark
+     *
+     * @param dark bool if the beer is set to dark
+     */
+    public void setDark() {
+    }
+
+    /**
+     * Getter for wort
+     *
+     * @return Beer's wort
+     */
+    public int getWort() {
+    }
+
+    /**
+     * Setter for wort
+     *
+     * @param wort int for the wort
+     */
+    public void setWort() {
+    }
+
+    /**
+     * Gets the volume of the liquid
+     *
+     * @return int for liquid's volume
+     */
+
+    @Override
+    public double getVolume() {
+    }
+
+    /**
+     * Getter for the alcohol content
+     * @return int representing alcohol content in volume percent
+     */
+
+    @Override
+    public double getAlcoholPercent() {
+    }
+
+    /**
+     * Bool method to determine whether a beer is alcoholic or not
+     * @return the liquids alcohol percentage as a double
+     */
+    @Override
+    public boolean isAlcoholic() {
+    }
+}
