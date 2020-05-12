fd4a1e87af62e359aae06aed6cc6717459cbde91
DominikSchick <dominik.schick@edu.campus02.at>
2020-05-12 16:24:32 +0200

    Implemented methods in TestSpirit.java

--- src/test/TestSpirit.java
+++ src/test/TestSpirit.java
@@ -1,14 +1,30 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 /**
  * Class for Testing
  */
  public class TestSpirit {
+     static Spirit laphroaig, havana, slivovic, zirbe, stroh, gin;
 
    /**
     * will execute operation before each test
     */
     @BeforeClass
     public static void setup() {
-
+        laphroaig = new Spirit(true, "malt", "Laphroaig",
+                new Liquid("whiskey", 0.1, 40));
+        havana = new Spirit(true, "Sugar Cane", "Havana Club 23",
+                new Liquid("rum", 0.1, 40));
+        slivovic = new Spirit(false, "plum", "slivovic",
+                new Liquid("slivovic", 0.1, 55));
+        zirbe = new Spirit(true, "grain", "Zirbe",
+                new Liquid("Zirbe", 0.1, 40));
+        stroh = new Spirit(false, "Sugar Cane", "Stroh",
+                new Liquid("Stroh 80", 0.1, 80));
+        gin = new Spirit(false, "juniper", "Hendricks",
+                new Liquid("Gin", 0.1, 40));
     }
 
     /**
@@ -17,7 +33,15 @@
      */
      @Test
      public void testConstructor() {
-
+           assertTrue(laphroaig.isDark());
+           assertFalse(slivovic.isDark());
+           assertEquals("malt", laphroaig.getMainIngredient());
+           assertEquals("Sugar Cane", havana.getMainIngredient());
+           assertEquals("slivovic", slivovic.getName());
+           assertEquals("Zirbe", zirbe.l.getName());
+           assertEquals("Stroh 80", stroh.l.getName());
+           assertEquals(0.1, stroh.getVolume());
+           assertEquals(40, zirbe.getAlcoholPercent());
      }
 
      /**
@@ -26,7 +50,7 @@
       */
       @Test
       public void testHardStuffToString() {
-
+            assertEquals("slivovic\nStroh\n", Spirit.hardStufftoString());
       }
 
       /**
@@ -35,7 +59,10 @@
        */
        @Test
        public void checkIfSpirit() {
-
+           assertThrows(IllegalArgumentException.class, () -> {
+               Spirit likör = new Spirit(false, "sugar", "xuxu", new Liquid("sugarsyrup", 0.4,
+                       25));
+           });
        }
 
        /**
@@ -45,7 +72,11 @@
         */
         @Test
         public void testSetDark() {
-
+            Spirit test = new Spirit(false, "testii", "testtest", (new Liquid("test",
+                    0.1, 40)));
+            assertFalse(test.isDark());
+            test.setDark(true);
+            assertTrue(test.isDark());
         }
 
         /**
@@ -55,6 +86,10 @@
          */
          @Test
          public void testSetMainIngredient() {
-
+             Spirit test = new Spirit(false, "testii", "testtest", (new Liquid("test",
+                     0.1, 40)));
+             assertEquals(test.getMainIngredient(), "testii");
+             test.setMainIngredient("testii2");
+             assertEquals(test.getMainIngredient(), "testii2");
          }
        }
