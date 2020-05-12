7e3c81905b87c7be96f3e1b56bbb99a4b9890493
thomasstrutz <62703418+thomasstrutz@users.noreply.github.com>
2020-05-12 09:53:06 +0200

    created the basic outline of the class, added empty methods and commented them

---
+++ src/main/java/Spirit.java
@@ -0,0 +1,66 @@
+public class Spirit extends SimpleDrink {
+    
+
+    /**
+     * Constructor for Spirit
+     * @param isDark bool for whether the spirit is dark or not
+     * @param mainIngredient the spirit's main ingredient
+     * @param name the spirit's name
+     * @param l the corresponding liquid for each spirit
+     * @throws IllegalArgumentException is thrown when a spirit with less than 40%
+     * alcohol content is created
+     */
+    Spirit(){       
+    }
+
+    /**
+     * Prints all the spirits with more then 50% alcohol content, from i to
+     * the end of the hardStuff list and puts a newline behind each spirits
+     * name
+     *
+     * @return return all the spirits with more then 50% alcohol content
+     */
+    public static String hardStufftoString(){
+    }
+
+    /**
+     * Getter method for the color of the spirit
+     *
+     * @return returns true if the drink is dark and false if the drink isn't
+     */
+    public boolean isDark() {
+    }
+
+    /**
+     * Setter method for the color of the spirit
+     *
+     * @param dark is the boolean that comes with the function call, true is
+     *             dark false isn't dark
+     */
+    public void setDark() {  
+    }
+
+    /**
+     * Getter method for the name of the main ingredient in the spirit
+     *
+     * @return returns the name of the main ingredient of the spirit
+     */
+    public String getMainIngredient() {
+    }
+
+    /**
+     * Setter method for the main ingredient in the spirit
+     *
+     * @param mainIngredient the name (as String) of the main ingredient in the
+     *                       spirit
+     */
+    public void setMainIngredient() {
+    }
+
+    /**
+     * getter for the name
+     * @return String representing the spirit's name
+     */
+    public String getName(){
+    }
+}
